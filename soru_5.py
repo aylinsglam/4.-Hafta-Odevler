a309a8ac694ee9c3a738ca5b30f068fccec1aae3
aylinsglam <73175714+aylinsglam@users.noreply.github.com>
2020-11-21 23:35:16 +0300

    Add files via upload

---
+++ soru_5.py
@@ -0,0 +1,11 @@
+a1=list()
+a2=list()
+for i in range(0,5):
+    x=input("İsim: ")
+    y=input("soyisim:")
+    z=input("Takım:")
+    if z=="Barcelona":
+        a1=[x,y,z]
+        a2.append(a1)
+print(a2)
+        
